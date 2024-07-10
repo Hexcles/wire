cfad49deac9f8c51b45a077394bd3487a6b83ba2
Robert Ma <robert.ma@faire.com>
2024-07-10 16:58:17 -0700

    Disable swift

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -64,7 +64,7 @@ include(":wire-schema-tests")
 include(":wire-swift-generator")
 include(":wire-test-utils")
 include(":wire-tests")
-if (startParameter.projectProperties.get("swift") != "false") {
+if (false) {
   include(":wire-runtime-swift")
   include(":wire-tests-swift")
   include(":wire-tests-swift:no-manifest")
